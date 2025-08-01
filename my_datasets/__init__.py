cb4df1c36c438286c7211c3b345c01c629ccb367
James Yu <james-yu@outlook.com>
2025-08-01 01:50:17 +0100

    check for ramdisks

--- my_datasets/__init__.py
+++ my_datasets/__init__.py
@@ -2,6 +2,7 @@ from typing import Literal
 from .gridwatch import GridwatchDataset
 from ._darcy_1d import Darcy1dDataset, generate_dataset as generate_darcy_1d
 from ._diffusion_pde import prep_diffusion_pde as diffusion_pde_preprocess, DiffusionPDEDataset, METADATA
+from os import path
 
 
 def get_dataset(dataset_name: str, *args, **kwargs):
@@ -10,7 +11,12 @@ def get_dataset(dataset_name: str, *args, **kwargs):
     elif dataset_name == "darcy_1d":
         return Darcy1dDataset(loc="./data/darcy_1d", *args, **kwargs)
     elif dataset_name in METADATA:
-        return DiffusionPDEDataset(ds_name=dataset_name, loc=f"/ramdisks/{dataset_name}", *args, **kwargs)
+        if path.exists(f"/ramdisks/{dataset_name}"):
+            print(f"GOING FOR `/ramdisks/{dataset_name}`!")
+            return DiffusionPDEDataset(ds_name=dataset_name, loc=f"/ramdisks/{dataset_name}", *args, **kwargs)
+        else:
+            print(f"GOING FOR `./data/{dataset_name}`!")
+            return DiffusionPDEDataset(ds_name=dataset_name, loc=f"./data/{dataset_name}", *args, **kwargs)
     raise ValueError()
 
 
