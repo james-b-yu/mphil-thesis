052873522abfda03762e31bd319285dfcda0d61b
James Yu <james-yu@outlook.com>
2025-08-06 14:57:43 +0100

    add new metrics

---
+++ test_sweep_darcy_1d_lens.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Define the list of len methods to loop through
+lens=("0005" "001" "002" "005" "010" "020" "050")
+
+# Outer loop for len methods
+for len in "${lens[@]}"
+do
+  # Inner loop for the run index (0, 1, 2)
+  for i in {0..2}
+  do
+     echo "==> Running test with len: ${len}, run: ${i}"
+     ./run.py test \
+         --config=./configurations/sweep_darcy_1d/${len}.yml \
+         --pth=./out_darcy_1d_${len}/ema_epoch_500.pth \
+         --n-steps=100 \
+         --n-batch-size=1000 \
+         --out-file=./test_sweep_darcy_1d/final_sweep-${len}-${i}.npz \
+         --stats-out=./test_sweep_darcy_1d/final_sweep-${len}-${i}.csv \
+         --weighting=linear-out
+  done
+done
