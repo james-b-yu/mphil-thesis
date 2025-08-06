9787c742df0e633151466edf29e95bab2cb320dd
James Yu <james-yu@outlook.com>
2025-08-06 16:12:03 +0100

    update gitignore

--- test_sweep_darcy_1d_lens.sh
+++ test_sweep_darcy_1d_lens.sh
@@ -12,11 +12,11 @@ do
      echo "==> Running test with len: ${len}, run: ${i}"
      ./run.py test \
          --config=./configurations/sweep_darcy_1d/${len}.yml \
-         --pth=./out_darcy_1d_${len}/ema_epoch_500.pth \
+         --pth=./out_darcy_1d_${len}/ema_epoch_1500.pth \
          --n-steps=100 \
-         --n-batch-size=1000 \
-         --out-file=./test_sweep_darcy_1d/final_sweep-${len}-${i}.npz \
-         --stats-out=./test_sweep_darcy_1d/final_sweep-${len}-${i}.csv \
+         --n-batch-size=1500 \
+         --out-file=./test_sweep_darcy_1d/final_sweep-${len}-${i}_epoch_1500.npz \
+         --stats-out=./test_sweep_darcy_1d/final_sweep-${len}-${i}_epoch_1500.csv \
          --weighting=linear-out \
          --method=em_2
   done
